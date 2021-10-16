8bfad042bd9aff74e6d409f6da13288cd7df4af6
Nisar Ahmed <53566249+nisarr@users.noreply.github.com>
2021-10-16 11:30:44 +0500

    Update input.blade.php

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -21,13 +21,14 @@
 ],
 'disableFieldError' => false,
 'hideErrorOnChange' => true,
+'parentClass' => ''
 ])
 
-<div class="form-group">
+<div class="form-group {{ $parentClass }}">
   @if($label) 
   <label id="{{ $labelOption['id'] }}" class="{{ $labelOption['class'] }}">{{ $label }}</label> 
   @endif
-  <input {{ $attributes }} id="{{ $id }}" name="{{ $name }}" class="lara-b4-input form-control @error($name) is-invalid @endif @if($hideErrorOnChange) lara-b4-input-hide-error @endif" placeholder="{{$placeholder ?? $label}}" type="{{ $type }}" value="@if($old){{old($name,$value)}}@else{{$value}}@endif">
+  <input {!! $attributes !!} id="{{ $id }}" name="{{ $name }}" class="lara-b4-input form-control @error($name) is-invalid @endif @if($hideErrorOnChange) lara-b4-input-hide-error @endif" placeholder="{{$placeholder ?? $label}}" type="{{ $type }}" value="@if($old){{old($name,$value)}}@else{{$value}}@endif">
   <x-lara-b4::input-error :error="($error ? $error : ( !$disableFieldError ? $name : ''))" />
   @if($help)
   <small id="{{ $helpOption['id'] }}" class="{{ $helpOption['class'] }}">{{ $help }}</small>
